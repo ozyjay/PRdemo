3186a2330c3865f90b80799abd000c98920b7b70
Lindsay Ward <lindsay.ward@jcu.edu.au>
2015-10-13 10:18:42 +1000

    code review 2 - refactored variable x

--- demoValidity.py
+++ demoValidity.py
@@ -6,20 +6,21 @@ __author__ = 'Jason Holdsworth'
 
 def get_valid_number(lower, upper, prompt="Enter number: "):
     valid = False
-    x = 0
+    number = 0
     while not valid:
         try:
-            x = int(input(prompt))
-            if x < lower or x > upper:
+            number = int(input(prompt))
+            if number < lower or number > upper:
                 print("Number is out of range")
             else:
                 valid = True
         except ValueError:
             print("Number is bad")
-    return x
+    return number
 
 
-# test
+# testing examples - comment out to run
+
 # age = get_valid_number(0, 120, "How old are you? ")
 # print("You are", age, "years old")
 # print("Finished")
